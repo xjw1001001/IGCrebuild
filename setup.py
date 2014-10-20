7035b6a23a130dcfa65e7a393f48796a1895ed1e
alex <argriffi@ncsu.edu>
2014-10-20 16:10:02 -0400

    infrastructure

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""
+Analysis of continuous time Markov chains on trees.
+
+"""
+
+DOCLINES = __doc__.split('\n')
+
+# This setup script is written according to
+# http://docs.python.org/2/distutils/setupscript.html
+#
+# It is meant to be installed through github using pip.
+
+from distutils.core import setup
+
+
+setup(
+        name='jsonctmctree',
+        version='0.1',
+        description=DOCLINES[0],
+        author='alex',
+        url='https://github.com/argriffing/jsonctmctree/',
+        download_url='https://github.com/argriffing/jsonctmctree/',
+        packages=['jsonctmctree'],
+        test_suite='nose.collector',
+        package_data={'jsonctmctree' : ['tests/test_*.py']},
+        )
