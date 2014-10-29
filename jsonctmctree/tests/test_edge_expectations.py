6ea0b90e78c2d3e992a7c97a9e319169dc83a008
alex <argriffi@ncsu.edu>
2014-10-29 18:41:50 -0400

    add some brute force testing

--- jsonctmctree/tests/test_edge_expectations.py
+++ jsonctmctree/tests/test_edge_expectations.py
@@ -4,8 +4,13 @@ Brute force test of weighted expectations on edges.
 """
 from __future__ import division, print_function, absolute_import
 
-import numpy as np
+from itertools import product
 
+import numpy as np
+from numpy.testing import assert_allclose
+
+from scipy.linalg import expm
+from scipy.sparse.linalg import expm_multiply
 from jsonctmctree.expect import process_json_in
 
 
@@ -17,6 +22,8 @@ def test_tiny_model():
     nedges = nnodes - 1
     
     for r in (0.1, 0.2):
+
+        print('*** tiny model with r =', r, '***')
         j_in = dict(
                 node_count = nnodes,
                 process_count = 1,
@@ -53,8 +60,12 @@ def test_simple_model():
     # The rate towards the decided state 2 is twice as fast
     # as the rate towards the decided state 1.
 
+    np.set_printoptions(precision=16)
+
     nnodes = 4
     nedges = nnodes - 1
+
+    print('*** simple model ***')
 
     j_in = dict(
             node_count = 4,
@@ -80,6 +91,67 @@ def test_simple_model():
                 [1],
                 [0],
                 [2]])
+
+    # Compute marginal distributions at internal nodes analytically.
+    nnodes = 4
+    nstates = 3
+    Q = np.array([
+        [-0.3, 0.1, 0.2],
+        [ 0.0, 0.0, 0.0],
+        [ 0.0, 0.0, 0.0]], dtype=float)
+    P = expm(Q)
+    Px = expm_multiply(Q, np.identity(nstates))
+    assert_allclose(P, Px)
+    print(P)
+    W = np.zeros((nnodes, nstates))
+    for assign in product(range(nstates), repeat=nnodes):
+        assign = list(assign)
+        # compute a likelihood
+        if assign[0] != 0:
+            continue
+        if assign[-1] != 2:
+            continue
+        lk = 1
+        for i, j in zip(assign[:-1], assign[1:]):
+            lk *= P[i, j]
+        for node, state in enumerate(assign):
+            W[node, state] += lk
+    print('brute force marginal state distributions at nodes:')
+    for node in range(nnodes):
+        print('node:', node)
+        print('distn:', W[node] / W[node].sum())
+    print()
+
+    for r in 0.1, 0.2:
+        print('*** brute force r = ', r, '***')
+        # Compute marginal distributions at internal nodes analytically.
+        nnodes = 4
+        nstates = 2
+        Q = np.array([
+            [-r, r],
+            [ 0, 0]], dtype=float)
+        P = expm(Q)
+        Px = expm_multiply(Q, np.identity(nstates))
+        assert_allclose(P, Px)
+        print(P)
+        W = np.zeros((nnodes, nstates))
+        for assign in product(range(nstates), repeat=nnodes):
+            assign = list(assign)
+            # compute a likelihood
+            if assign[0] != 0:
+                continue
+            if assign[-1] != 1:
+                continue
+            lk = 1
+            for i, j in zip(assign[:-1], assign[1:]):
+                lk *= P[i, j]
+            for node, state in enumerate(assign):
+                W[node, state] += lk
+        print('brute force marginal state distributions at nodes:')
+        for node in range(nnodes):
+            print('node:', node)
+            print('distn:', W[node] / W[node].sum())
+        print()
 
     j_out = process_json_in(j_in)
     print(j_out)
