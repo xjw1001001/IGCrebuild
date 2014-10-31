253d86b9da461939fa1bae20c25bdf8e3f8a8391
alex <argriffi@ncsu.edu>
2014-10-30 23:30:43 -0400

    add commented out sympy code to check jordan blocks

--- jsonctmctree/tests/test_latent_variable.py
+++ jsonctmctree/tests/test_latent_variable.py
@@ -11,18 +11,10 @@ from numpy.testing import assert_equal, assert_array_less
 
 from jsonctmctree.expect import process_json_in
 
+from sympy import Matrix
 
-def test_latent_variable():
-    
-    # This model is bivariate.
-    # Both variables evolve deterministically along a path,
-    # but each variable may also convert to its neighbor state.
-    n = 3
-    norm_rate = 0.1
-    conv_rate = 0.01
-    state_space_shape = [n, n]
-    states = product(range(n), repeat=2)
-    rate_info = []
+
+def _gen_info(states, norm_rate, conv_rate):
     for trans in permutations(states, 2):
         sa, sb = trans
         ai, aj = sa
@@ -40,7 +32,52 @@ def test_latent_variable():
             rate += conv_rate
             expect += conv_rate
         if rate:
-            rate_info.append((sa, sb, rate, expect / rate))
+            yield sa, sb, rate, expect / rate
+
+
+def _show_jordan_blocks(R):
+    assert_equal(len(R.shape), 2)
+    assert_equal(R.shape[0], R.shape[1])
+    n = R.shape[0]
+    entries = R.flatten().tolist()
+    m = Matrix(n, n, entries)
+    P, J = m.jordan_form()
+    print('rate matrix:')
+    print(m)
+    print('jordan blocks:')
+    print(J)
+
+
+def test_latent_variable():
+    
+    # This model is bivariate.
+    # Both variables evolve deterministically along a path,
+    # but each variable may also convert to its neighbor state.
+    n = 3
+    norm_rate = 0.1
+    conv_rate = 0.01
+    state_space_shape = [n, n]
+    nstates = np.prod(state_space_shape)
+    states = list(product(range(n), repeat=2))
+    rate_info = list(_gen_info(states, norm_rate, conv_rate))
+
+    #_show_jordan_blocks(np.array([
+        #[-1, 1, 0],
+        #[0, -1, 1],
+        #[1, 0, -1]], dtype=int))
+
+    # Check the jordan form using sympy.
+    """
+    mstates = list(product(range(3), repeat=2))
+    mnstates = len(mstates)
+    rmap = dict((s, i) for i, s in enumerate(mstates))
+    R = np.zeros((mnstates, mnstates), dtype=int)
+    for sa, sb, r, x in _gen_info(mstates, 1, 1):
+        print(sa, sb, r)
+        R[rmap[sa], rmap[sb]] = r
+    R -= np.diag(R.sum(axis=1))
+    _show_jordan_blocks(R)
+    """
 
     nnodes = 5
     nedges = nnodes - 1
