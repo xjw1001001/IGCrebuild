ff7ed5dee7fbd66973369240699083b8828746da
alex <argriffi@ncsu.edu>
2014-11-03 18:09:50 -0500

    move sampling functions into the main code

--- jsonctmctree/tests/test_prior_expectations.py
+++ jsonctmctree/tests/test_prior_expectations.py
@@ -11,49 +11,21 @@ from numpy.testing import assert_allclose
 import scipy.linalg
 
 from jsonctmctree.expect import process_json_in
+from jsonctmctree.sampling import(
+        assert_symmetric_matrix,
+        sample_symmetric_rates,
+        sample_distn,
+        sample_time_reversible_rate_matrix,
+        sample_time_nonreversible_rate_matrix,
+        )
 
-
-def assert_symmetric_matrix(M):
-    assert_allclose(M, M.T)
-
-def _sample_symmetric_rates(n):
-    X = np.random.randn(n, n)
-    S = np.exp(X + X.T)
-    np.fill_diagonal(S, 0)
-    return S
-
-def _sample_distn(n):
-    d = np.exp(np.random.randn(n))
-    return d / d.sum()
-
-def _sample_time_reversible_rate_matrix(nstates):
-    d = _sample_distn(nstates)
-    Q = _sample_symmetric_rates(nstates) * d
-    Q -= np.diag(Q.sum(axis=1))
-    assert_allclose(Q.sum(axis=1), 0, atol=1e-13)
-    assert_allclose(d.dot(Q), 0, atol=1e-13)
-    P = np.diag(d)
-    assert_symmetric_matrix(P.dot(Q))
-    return Q, d
-
-def _sample_time_nonreversible_rate_matrix(nstates):
-    Q = np.exp(np.random.randn(nstates, nstates))
-    Q -= np.diag(Q.sum(axis=1))
-    w, V = scipy.linalg.eig(Q, left=True, right=False)
-    i = np.argmin(np.abs(w))
-    d = V[:, i].real
-    d /= d.sum()
-    assert_allclose(Q.sum(axis=1), 0, atol=1e-13)
-    assert_allclose(d.dot(Q), 0, atol=1e-13)
-    P = np.diag(d)
-    return Q, d
 
 def _compute_expectations(Q, d, dwell_expect, root_expect):
     nnodes = 5
     nedges = nnodes - 1
     nstates = d.shape[0]
     states = range(nstates)
-    edge_rates = _sample_distn(nedges)
+    edge_rates = sample_distn(nedges)
     state_pairs = list(permutations(range(nstates), 2))
     ntrans = len(state_pairs)
     transition_rates = [Q[i, j] for i, j in state_pairs]
@@ -103,8 +75,8 @@ def _compute_expectations(Q, d, dwell_expect, root_expect):
 def test_prior_expectations():
     nstates = 4
     for fn_sample in (
-        _sample_time_reversible_rate_matrix,
-        _sample_time_nonreversible_rate_matrix,
+        sample_time_reversible_rate_matrix,
+        sample_time_nonreversible_rate_matrix,
         ):
 
         Q, d = fn_sample(nstates)
