853a36fb186c97a286786c282c5e8ad33f8c2141
alex <argriffi@ncsu.edu>
2014-11-11 13:44:59 -0500

    add more random samples to one of the unit tests

--- jsonctmctree/tests/test_identifiability_puzzle.py
+++ jsonctmctree/tests/test_identifiability_puzzle.py
@@ -31,31 +31,33 @@ def test_identifiability():
     nstates = np.prod(state_space_shape)
     bivariate_states = list(product(range(n), repeat=2))
     bivariate_state_pairs = list(permutations(bivariate_states, 2))
-    for fn_sample in (
-        sample_time_reversible_rate_matrix,
-        sample_time_nonreversible_rate_matrix,
-        ):
-        Q, d = fn_sample(n)
-        P = scipy.linalg.expm(Q)
-        for tau in 0, 0.1, 2.0:
+    nrepeats = 10
+    for repeat_index in range(nrepeats):
+        for fn_sample in (
+            sample_time_reversible_rate_matrix,
+            sample_time_nonreversible_rate_matrix,
+            ):
+            Q, d = fn_sample(n)
+            P = scipy.linalg.expm(Q)
+            for tau in 0, 0.1, 2.0:
 
-            # Compute the bivariate process.
-            Q_bivariate, d_bivariate = geneconvify(Q, d, tau)
-            P_bivariate = scipy.linalg.expm(Q_bivariate)
+                # Compute the bivariate process.
+                Q_bivariate, d_bivariate = geneconvify(Q, d, tau)
+                P_bivariate = scipy.linalg.expm(Q_bivariate)
 
-            # Check that for each (i, i) initial state,
-            # the marginal transition matrix is equal
-            # to the univariate transition matrix.
-            P_marginal_0 = np.zeros((n, n), dtype=float)
-            P_marginal_1 = np.zeros((n, n), dtype=float)
-            for a in range(n):
-                s_initial = (a, a)
-                i = np.ravel_multi_index(s_initial, state_space_shape)
-                for s in bivariate_states:
-                    u, v = s
-                    j = np.ravel_multi_index(s, state_space_shape)
-                    P_marginal_0[a, u] += P_bivariate[i, j]
-                    P_marginal_1[a, v] += P_bivariate[i, j]
+                # Check that for each (i, i) initial state,
+                # the marginal transition matrix is equal
+                # to the univariate transition matrix.
+                P_marginal_0 = np.zeros((n, n), dtype=float)
+                P_marginal_1 = np.zeros((n, n), dtype=float)
+                for a in range(n):
+                    s_initial = (a, a)
+                    i = np.ravel_multi_index(s_initial, state_space_shape)
+                    for s in bivariate_states:
+                        u, v = s
+                        j = np.ravel_multi_index(s, state_space_shape)
+                        P_marginal_0[a, u] += P_bivariate[i, j]
+                        P_marginal_1[a, v] += P_bivariate[i, j]
 
-            assert_allclose(P_marginal_0, P)
-            assert_allclose(P_marginal_1, P)
+                assert_allclose(P_marginal_0, P)
+                assert_allclose(P_marginal_1, P)
