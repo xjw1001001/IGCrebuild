4ad86979d3c9d8d2d52a93c381cfc2df0434852e
alex <argriffi@ncsu.edu>
2014-11-12 12:09:03 -0500

    move some code around

--- jsonctmctree/tests/test_identifiability_puzzle.py
+++ jsonctmctree/tests/test_identifiability_puzzle.py
@@ -21,8 +21,85 @@ from jsonctmctree.sampling import(
         sample_distn,
         sample_time_reversible_rate_matrix,
         sample_time_nonreversible_rate_matrix,
-        geneconvify,
+        _brute_force_equilibrium,
         )
+
+# FIXME
+# I understand why the 'gene conversion' dependence parameter is not
+# identifiable when only the endpoints of a single branch are observable,
+# but I don't understand why it is still not identifiable when the
+# gene conversion rate depends on the distinction between
+# synonymous vs. nonsynonymous substitution.
+# This distinction can be thought of as partitioning the univariate states
+# and scaling rates between partitions differently than the
+# rates within states in the same partition.
+
+
+def _gen_univariate_transitions(sa, sb):
+    for a, b in zip(sa, sb):
+        if a != b:
+            yield a, b
+
+
+def _geneconvify(Q_in, d_in, tau):
+    """
+    Combine two univariate processes into a bivariate process.
+
+    Parameters
+    ----------
+    Q_in : 2d ndarray (n, n)
+        univariate rate matrix
+    d_in : 1d ndarray (n, )
+        equilibrium distribution
+    tau : float
+        extra homogenization rate
+
+    Returns
+    -------
+    Q_out : 2d ndarray (n*n, n*n)
+        bivariate rate matrix
+    d_out : 1d ndarray (n*n, )
+        equilibrium distribution
+
+    """
+    assert_equal(len(d_in.shape), 1)
+    assert_equal(len(Q_in.shape), 2)
+    n = d_in.shape[0]
+    state_space_shape = (n, n)
+    assert_equal(Q_in.shape, state_space_shape)
+    nstates = np.prod(state_space_shape)
+    Q_out = np.zeros((nstates, nstates), dtype=float)
+    d_out = np.zeros((nstates, ), dtype=float)
+    bivariate_states = list(product(range(n), repeat=2))
+    bivariate_state_pairs = list(permutations(bivariate_states, 2))
+    for sa, sb in bivariate_state_pairs:
+        rate = 0
+        t = list(_gen_univariate_transitions(sa, sb))
+        if len(t) == 1:
+            a, b = t[0]
+            rate += Q_in[a, b]
+        if sb[0] == sb[1]:
+            if sa[0] == sb[0] or sa[1] == sb[0]:
+                rate += tau
+        if rate:
+            i = np.ravel_multi_index(sa, state_space_shape)
+            j = np.ravel_multi_index(sb, state_space_shape)
+            Q_out[i, j] = rate
+    Q_out -= np.diag(Q_out.sum(axis=1))
+    d_out = _brute_force_equilibrium(Q_out)
+    # Check that the marginal equilbrium distributions
+    # of the bivariate process are equal to the equilibrium distribution
+    # of the underlying process.
+    m0 = np.zeros(n, dtype=float)
+    m1 = np.zeros(n, dtype=float)
+    for s in bivariate_states:
+        a, b = s
+        p = d_out[np.ravel_multi_index(s, state_space_shape)]
+        m0[a] += p
+        m1[b] += p
+    assert_allclose(m0, d_in)
+    assert_allclose(m1, d_in)
+    return Q_out, d_out
 
 
 def test_identifiability():
@@ -43,7 +120,7 @@ def test_identifiability():
                 for tau in 0, 0.1, 2.0:
 
                     # Compute the bivariate process.
-                    Q_bivariate, d_bivariate = geneconvify(Q, d, tau)
+                    Q_bivariate, d_bivariate = _geneconvify(Q, d, tau)
                     P_bivariate = scipy.linalg.expm(scale * Q_bivariate)
 
                     # Check that for each (i, i) initial state,
