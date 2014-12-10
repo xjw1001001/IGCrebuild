ea68958eef1f20f9400dac8ba53256cfeeaf48d4
alex <argriffi@ncsu.edu>
2014-12-10 14:59:12 -0500

    add a json thing

--- docs/tut05/make-rate-matrix.py
+++ docs/tut05/make-rate-matrix.py
@@ -6,13 +6,21 @@ print('row states')
 lines = []
 for i in range(n):
     line = ','.join(('[%d]' % i) for j in range(n) if i != j)
-    lines.append('[' + line + ']')
+    lines.append(line)
 print(',\n'.join(lines))
 
 print('column_states')
 lines = []
 for i in range(n):
     line = ','.join(('[%d]' % j) for j in range(n) if i != j)
-    lines.append('[' + line + ']')
+    lines.append(line)
 print(',\n'.join(lines))
+
+print('rates')
+lines = []
+for i in range(n):
+    line = ','.join('1' for j in range(n) if i != j)
+    lines.append(line)
+print(',\n'.join(lines))
+
 
