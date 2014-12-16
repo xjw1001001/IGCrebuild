09f988ab8e5d11b0939dbb1bc7ff830c660f8bc6
alex <argriffi@ncsu.edu>
2014-12-15 19:21:05 -0500

    add more requests

--- docs/tut11/make-arrays.py
+++ docs/tut11/make-arrays.py
@@ -69,5 +69,71 @@ def main():
     print()
 
     print(rates)
+    print()
+
+    # Now let's create some transition reductions:
+    # synonymous, nonsynonymous, gain-of-tolerance, loss-of-tolerance
+    forward_trans_syn = [(0, 1), (2, 3), (4, 5)]
+    backward_trans_syn = [(b, a) for a, b in forward_trans_syn]
+    forward_trans_non = [(0, 2), (2, 4), (1, 3), (3, 5)]
+    backward_trans_non = [(b, a) for a, b in forward_trans_non]
+    trans_syn = forward_trans_syn + backward_trans_syn
+    trans_non = forward_trans_non + backward_trans_non
+    syn_triples = []
+    non_triples = []
+    gain_triples = []
+    loss_triples = []
+    for sa in legit_states:
+        for sb in legit_states:
+            diffs = get_diffs(sa, sb)
+            if len(diffs) != 1:
+                continue
+            i, x, y = diffs[0]
+            if i == 0:
+                if (x, y) in trans_syn:
+                    syn_triples.append((sa, sb, 1))
+                elif (x, y) in trans_non:
+                    non_triples.append((sa, sb, 1))
+            else:
+                if y:
+                    gain_triples.append((sa, sb, 1))
+                else:
+                    loss_triples.append((sa, sb, 1))
+
+    print('transition_rates syn:')
+    rows, cols, weights = zip(*syn_triples)
+    print(',\n'.join(str(s) for s in rows))
+    print()
+    print(',\n'.join(str(s) for s in cols))
+    print()
+    print(weights)
+    print()
+
+    print('transition_rates non:')
+    rows, cols, weights = zip(*non_triples)
+    print(',\n'.join(str(s) for s in rows))
+    print()
+    print(',\n'.join(str(s) for s in cols))
+    print()
+    print(weights)
+    print()
+
+    print('transition_rates gain:')
+    rows, cols, weights = zip(*gain_triples)
+    print(',\n'.join(str(s) for s in rows))
+    print()
+    print(',\n'.join(str(s) for s in cols))
+    print()
+    print(weights)
+    print()
+
+    print('transition_rates loss:')
+    rows, cols, weights = zip(*loss_triples)
+    print(',\n'.join(str(s) for s in rows))
+    print()
+    print(',\n'.join(str(s) for s in cols))
+    print()
+    print(weights)
+    print()
 
 main()
