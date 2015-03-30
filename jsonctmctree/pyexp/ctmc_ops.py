48e027575bf049b22c749f60ab1d7a26619b9732
alex <argriffi@ncsu.edu>
2015-03-30 17:24:29 -0400

    more work on linear operators with caching at various levels

--- jsonctmctree/pyexp/ctmc_ops.py
+++ jsonctmctree/pyexp/ctmc_ops.py
@@ -9,6 +9,8 @@ from __future__ import division, print_function, absolute_import
 
 import numpy as np
 
+from .experimental import IterationStash
+
 
 __all__ = ['RdOperator', 'RdcOperator', 'RdCOperator']
 
@@ -29,6 +31,7 @@ class _HighLevelInterface(object):
 
     def dot(self, other):
         return self._matmat(other)
+
 
 
 class _ConcreteInterface(object):
@@ -64,6 +67,28 @@ class _ConcreteInterface(object):
         return self._transpose()
 
 
+class _VanillaAdjointOperator(_HighLevelInterface):
+    # The forward operator is expected to meet a few extra conditions.
+    # It should be real-valued (so the adjoint is the transpose).
+    # It should also be square but not necessarily symmetric.
+    # This class is not mixed with _ConcreteInterface.
+    def __init__(self, L):
+        self._L = L
+        self.args = (L, )
+    @property
+    def shape(self):
+        return self._L.shape
+    @property
+    def dtype(self):
+        return self._L.dtype
+    def _matmat(self, other):
+        return self._L._my_adjoint_matmat(other)
+    def _adjoint(self):
+        return self._L
+    def _transpose(self):
+        return self._L
+
+
 class _ExtendedAdjointOperator(_HighLevelInterface):
     # The forward operator is expected to meet a few extra conditions.
     # It should be real-valued (so the adjoint is the transpose),
@@ -93,6 +118,27 @@ class _ExtendedAdjointOperator(_HighLevelInterface):
         return self._L
     def _transpose(self):
         return self._L
+
+
+class PowerOperator(_HighLevelInterface):
+    def __init__(self, L, p):
+        self._L = L
+        self._p = p
+        self.__adj = None
+    def _matmat(self, other):
+        for i in range(self._p):
+            other = self._L.dot(other)
+        return other
+    def _my_adjoint_matmat(self, other):
+        for i in range(self._p):
+            other = self._L.H.dot(other)
+        return other
+    def _transpose(self):
+        if self.__adj is None:
+            self.__adj = _VanillaAdjointOperator(self)
+        return self.__adj
+    def _adjoint(self):
+        return self._transpose()
 
 
 class _ExtendedMatrixOperator(_HighLevelInterface, _ConcreteInterface):
@@ -267,3 +313,106 @@ class RdCOperator(_HighLevelInterface, _ConcreteInterface):
         M[n:, :] = (self._Rd._my_adjoint_matmat(other[n:, :]) +
                     self._CH.dot(other[:n, :]))
         return M
+
+
+def _expm_product_helper(A, mu, iteration_stash, t, B):
+    # Estimate expm(t*M).dot(B).
+    # A = M - mu*I
+    # mu = mean(trace(M))
+    # The iteration stash helps to compute numbers of iterations to use.
+    # t is a scaling factor.
+    # B is the input matrix for the linear operator.
+
+    # Compute some input-dependent constants.
+    tol = np.ldexp(1, -53)
+    n0 = B.shape[1]
+    m, s = forward_iteration_stash.fragment_3_1(n0, t)
+
+    # Get the lapack function for computing matrix norms.
+    lange, = get_lapack_funcs(('lange',), (B,))
+
+    F = B
+    eta = np.exp(t*mu / float(s))
+    for i in range(s):
+        c1 = lange('i', B)
+        for j in range(m):
+            coeff = t / float(s*(j+1))
+            B = coeff * A.dot(B)
+            c2 = lange('i', B)
+            F = F + B
+            if c1 + c2 <= tol * lange('i', F):
+                break
+            c1 = c2
+        F = eta * F
+        B = F
+    return F
+
+
+def _Propagator(object):
+    """
+    Wraps a linear operator.
+
+    Given a linear operator L and a scaling factor t,
+    this provides the operator (and adjoint operator)
+    corresponding to the matrix exponential of the scaled operator L*t.
+    It is intended to be wrapped by a function
+    that provides the corresponding operator specific to a given t.
+
+    The point of this class is basically to cache properties
+    that are the same across different values of t.
+
+    """
+    def __init__(self, A, mu):
+        # A = M - mu*I is an abstract linear operator
+        # whose 1-norm is directly accessible.
+        # mu is the mean trace of M.
+        self._A = A
+        self._mu = mu
+        self._forward_iteration_stash = None
+        self._adjoint_iteration_stash = None
+
+    def _parameterized_matvec(self, t, B):
+        # Approximate expm(M*t).dot(B).
+        # t is a scaling factor of L
+        # B the input matrix of the linear function
+        if self._forward_iteration_stash is None:
+            self._forward_iteration_stash = IterationStash(self._A)
+        return _expm_product_helper(
+                self._A, self._mu, self._forward_iteration_stash, t, B)
+
+    def _parameterized_adjoint_matvec(self, t, B):
+        # Approximate expm(M.H*t).dot(B).
+        # t is a scaling factor of L
+        # B the input matrix of the adjoint linear function
+        if self._adjoint_iteration_stash is None:
+            self._adjoint_iteration_stash = IterationStash(self._A.H)
+        return _expm_product_helper(
+                self._A.H, self._mu, self._adjoint_iteration_stash, t, B)
+
+
+def MatrixExponential(_HighLevelInterface):
+    # The input is already a propagator; this just scales by a specific t.
+    def __init__(self, P, t):
+        # P is a propagator
+        # t is the scaling factor
+        self._P = P
+        self._t = t
+        self.__adj = None
+
+    def _matvec(self, B):
+        # B is the input matrix of the linear function.
+        return self._P._parameterized_matvec(self._t, B)
+
+    def _my_adjoint_matvec(self, B):
+        # B is the input matrix of the adjoint linear function.
+        return self._P._parameterized_adjoint_matvec(self._t, B)
+
+    def _transpose(self):
+        # This uses a vanilla adjoint because norms are unavailable.
+        if self.__adj is None:
+            self.__adj = _VanillaAdjointOperator(self)
+        return self.__adj
+
+    def _adjoint(self):
+        # This is the same as the transpose because the entries are real.
+        return self._transpose()
