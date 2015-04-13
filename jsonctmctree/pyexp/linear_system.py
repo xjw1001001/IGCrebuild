9d9c36c04128029a8ecf77ded5821985f9153f80
alex <argriffi@ncsu.edu>
2015-04-13 19:38:00 -0400

    allow optional control over dense vs abstract operator choice in linear systems

--- jsonctmctree/pyexp/linear_system.py
+++ jsonctmctree/pyexp/linear_system.py
@@ -4,6 +4,7 @@
 from __future__ import division, print_function, absolute_import
 
 import numpy as np
+from numpy.testing import assert_
 
 from .ctmc_ops import Propagator, ExplicitPropagator, RdOperator
 
@@ -22,8 +23,35 @@ class LinearSystem(object):
     gives the probability of ending in state j after time t conditional
     on starting at state i.
 
+    The style hint determines whether to use a dense vs. an abstract
+    linear operator.  Note that regardless of the style hint,
+    the input should still be a scipy sparse matrix.
+
     """
-    def __init__(self, R):
+    def __init__(self, R, style='auto'):
+
+        # Input validation.
+        assert_(style in {'auto', 'dense', 'abstract'})
+        if len(R.shape) != 2:
+            raise ValueError
+        if R.shape[1] != R.shape[0]:
+            raise ValueError
+
+        # Determine whether to use a dense matrix or not.
+        n = R.shape[0]
+        if style == 'auto':
+            auto_threshold = 100
+            if n < auto_threshold:
+                use_dense_matrix = True
+            else:
+                use_dense_matrix = False
+        elif style == 'dense':
+            use_dense_matrix = True
+        elif style == 'abstract':
+            use_dense_matrix = False
+        else:
+            raise ValueError
+
         # R : sparse square matrix of non-negative off-diagonal rates
         self.shape = R.shape
         self.dtype = R.dtype
@@ -32,7 +60,7 @@ class LinearSystem(object):
         exit_rates = R.sum(axis=1).A.ravel()
 
         # Determine whether to use abstract or explicit linear operators.
-        if exit_rates.shape[0] < 100:
+        if use_dense_matrix:
             self._Q = R.A - np.diag(exit_rates)
             self._P = ExplicitPropagator(self._Q)
         else:
