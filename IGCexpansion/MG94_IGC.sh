45d0bb6b579d6182d92f0cd202d08aea94166077
xji3 <xji3@ncsu.edu>
2016-05-10 17:48:01 -0400

    push before running on cluster

---
+++ IGCexpansion/MG94_IGC.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sbatch -o IGC-%j.out --mail-type=FAIL --mail-user=xji3@ncsu.edu ./ShFiles/MG94_YLR406C_YDL075W_nonclock.sh 
+sbatch -o IGC-%j.out --mail-type=FAIL --mail-user=xji3@ncsu.edu ./ShFiles/MG94_YER131W_YGL189C_nonclock.sh 
+sbatch -o IGC-%j.out --mail-type=FAIL --mail-user=xji3@ncsu.edu ./ShFiles/MG94_YML026C_YDR450W_nonclock.sh 
+sbatch -o IGC-%j.out --mail-type=FAIL --mail-user=xji3@ncsu.edu ./ShFiles/MG94_YNL301C_YOL120C_nonclock.sh 
+sbatch -o IGC-%j.out --mail-type=FAIL --mail-user=xji3@ncsu.edu ./ShFiles/MG94_YNL069C_YIL133C_nonclock.sh 
+sbatch -o IGC-%j.out --mail-type=FAIL --mail-user=xji3@ncsu.edu ./ShFiles/MG94_YMR143W_YDL083C_nonclock.sh 
+sbatch -o IGC-%j.out --mail-type=FAIL --mail-user=xji3@ncsu.edu ./ShFiles/MG94_YJL177W_YKL180W_nonclock.sh 
+sbatch -o IGC-%j.out --mail-type=FAIL --mail-user=xji3@ncsu.edu ./ShFiles/MG94_YBR191W_YPL079W_nonclock.sh 
+sbatch -o IGC-%j.out --mail-type=FAIL --mail-user=xji3@ncsu.edu ./ShFiles/MG94_YER074W_YIL069C_nonclock.sh 
+sbatch -o IGC-%j.out --mail-type=FAIL --mail-user=xji3@ncsu.edu ./ShFiles/MG94_YDR418W_YEL054C_nonclock.sh 
+sbatch -o IGC-%j.out --mail-type=FAIL --mail-user=xji3@ncsu.edu ./ShFiles/MG94_YBL087C_YER117W_nonclock.sh 
+sbatch -o IGC-%j.out --mail-type=FAIL --mail-user=xji3@ncsu.edu ./ShFiles/MG94_YLR333C_YGR027C_nonclock.sh 
+sbatch -o IGC-%j.out --mail-type=FAIL --mail-user=xji3@ncsu.edu ./ShFiles/MG94_YMR142C_YDL082W_nonclock.sh 
+sbatch -o IGC-%j.out --mail-type=FAIL --mail-user=xji3@ncsu.edu ./ShFiles/MG94_YER102W_YBL072C_nonclock.sh 
